e2fb6b15c486e1fe1bfca85aa268d79af10af8dc
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-04-18 01:37:19 +0100

    Add files via upload

--- TurtleCount.lua
+++ TurtleCount.lua
@@ -81,7 +81,7 @@ function TurtleCount:ServerInfo()
 end
 
 function TurtleCount:RefreshTime()
-    refreshTime = GetTime() + 10
+    refreshTime = GetTime() + 60
 end
 
 -- Examples of Turtle WoW Server Info:
